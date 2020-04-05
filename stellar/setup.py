9c5592db390d48303cc927d3ed31101dbefd89f8
Zero <zero1729@protonmail.com>
2020-04-05 06:07:50 +0100

    misc: tooke out the setup in 'tools'
    The 'tools' folder is now properly bundled with the others under the 'stellar' namespace.

--- stellar/setup.py
+++ stellar/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name="stellar",
     version='0.8.0',
-    description="Rocket lang 'stellar' utilities",
+    description="Rocket lang 'stellar'",
     author="Zero-1729",
     author_email="zero1729@protonmail.com",
     packages=find_packages(),
