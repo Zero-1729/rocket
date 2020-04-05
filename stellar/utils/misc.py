a18e578c11fb86a4a355389c99d00b4099a09f57
Zero <zero1729@protonmail.com>
2020-04-05 06:19:49 +0100

    misc: properly styled 'misc' module file
    It is now more consistent with the other files in other modules.

--- stellar/utils/misc.py
+++ stellar/utils/misc.py
@@ -1,20 +1,26 @@
-# if p is negative then
-# (p * -1) - p will be equal to twice the absolute value of p (aka |p| ** 2), where x != 0
-#
-# Proof:
-#   Assume x = -x
-#
-#   (-x * -1) - (-x)
-#   = x - (-x)
-#   = x + x
-#   2x (hence |x| * 2)
-#
-# Now assume x = x
-#
-#       (x * -1) - (x)
-#       -x - x
-#       -2x (hence -|x| * 2 and not |x| * 2)
-#
-# QED
+# Utilities fns to reduce code redundancy
+# Integer Negativity test
 def isValNeg(x):
+    # Preamble:
+    #
+    #   if p is negative then
+    #   (p * -1) - p will be equal to twice the absolute value of p (aka |p| ** 2), where x != 0
+    #
+    # Proof:
+    #
+    #   Assume x = -x    (negative)
+    #
+    #   (-x * -1) - (-x)
+    #   = x - (-x)
+    #   = x + x
+    #   2x (hence |x| * 2)
+    #
+    #   Now assume x = x (positive)
+    #
+    #   (x * -1) - (x)
+    #   -x - x
+    #   -2x (hence -|x| * 2 and not |x| * 2)
+    #
+    # QED
     return (((x * -1) - x) == 2 * abs(x)) and (not x == 0)
+
