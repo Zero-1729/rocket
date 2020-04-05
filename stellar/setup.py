bb50dda1072a07d121e3dd66d956ac27394a156e
Zero <zero1729@protonmail.com>
2020-04-05 06:51:36 +0100

    stellar: bumped package ver number
    - Since our 'core' modules are also a part of the 'stellar' package, and we fixed 'utils' being included, it is only logical to bump the ver number to indicate these changes.

--- stellar/setup.py
+++ stellar/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="stellar",
-    version='0.8.0',
+    version='0.8.3',
     description="Rocket lang 'stellar'",
     author="Zero-1729",
     author_email="zero1729@protonmail.com",
