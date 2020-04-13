5286d11980eb27a1137dcc5a3d49c9eb097d0f62
Zero <zero1729@protonmail.com>
2020-04-13 15:13:16 +0100

    misc: cleaned setup file

--- stellar/setup.py
+++ stellar/setup.py
@@ -4,11 +4,8 @@ from setuptools import find_packages
 
 setup(name="stellar",
     version='0.8.3',
-    description="Rocket lang 'stellar'",
     description="Rocket lang 'stellar' interpreter modules",
     author="Zero-1729",
     author_email="zero1729@protonmail.com",
     packages=find_packages(),
     license="MIT")
-    license="MIT",
-    python_requires='>=3.7')
